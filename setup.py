f395da2cf4afeb316170d558efb7721b9daa9f48
Jia Lu <156269940+jialubio@users.noreply.github.com>
2024-10-04 15:27:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-# in terminal, run: python setup.py install
-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ML',  # Name of your project
-    description='This code uses ML surrogate to accelerate PDE simulation. The overall workflow includes 1. training a ML surrogate on PDE nuemrical simulation data; 2. Computational validatio by using ML inference results against PDE simulation; 3. Constructing a validate dataset and inferring patterning rules.',
+    description='Machine learning code for Lu, Jia, et al. Decoding pattern formation rules by integrating mechanistic modeling and deep learning. bioRxiv (2024)',
     author='Jia Lu',
-    author_email='jia.lu@duke.edu',
-    # url='https://doi.org/10.1101/2024.09.02.610872', # url to the pre-print
-    packages=ML,  
-    install_requires=['numpy', 'pandas', 'sklearn', 'torch', 'tqdm', 'matplotlib'],
+    packages=find_packages(),  
+    install_requires=['numpy', 
+                      'pandas', 
+                      'sklearn', 
+                      'torch', 
+                      'tqdm', 
+                      'matplotlib'],
     python_requires='>=3.8',
 )
